8f8223d037febee4ce96aed56f54a755680d3584
MikelTozza <mikel.tozza.muniz@gmail.com>
2024-05-02 15:30:36 -0400

    Updated the project structure

---
+++ src/Bluetooth/BlueToothDevices.h
@@ -0,0 +1,28 @@
+#ifndef BLUETOOTH_DEVICES_H
+#define BLUETOOTH_DEVICES_H
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <bluetoothapis.h>
+
+
+class BlueToothDevices {
+public:
+    struct DeviceInfo {
+        BLUETOOTH_DEVICE_INFO deviceInfo;
+        std::wstring name;
+        std::wstring address;
+        ULONG deviceClass;
+        bool authenticated;
+        bool connected;
+        bool handsfreeTelephonySupported;
+    };
+
+    std::vector<DeviceInfo> getBluetoothDevices();
+    bool checkHandsfreeTelephonySupport(const BLUETOOTH_DEVICE_INFO& deviceInfo);
+    bool setHandsfreeTelephonyServiceState(const DeviceInfo& device, bool enable);
+    bool isCorrectRadio(HANDLE radioHandle, const DeviceInfo& device);
+    std::wstring formatBluetoothAddress(BLUETOOTH_ADDRESS address);
+};
+
+#endif // BLUETOOTH_DEVICES_H
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include "Bluetooth/BlueToothDevices.h"
+
+
+int main()
+{
+    BlueToothDevices btObj;
+    auto devices = btObj.getBluetoothDevices();
+    if (devices.empty())
+    {
+        std::cout << "No Bluetooth devices found." << std::endl;
+    }
+    else
+    {
+        for (const auto &device : devices)
+        {
+            std::wcout << L"Found Device: " << std::setw(27) << std::left << device.name << L" | "
+                       << L"Address: " << std::setw(17) << std::left << device.address << L" | "
+                       << L"Handsfree Telephony Supported: " << (device.handsfreeTelephonySupported ? L"Yes" : L"No") << std::endl;
+        }
+    }
+
+    for (const auto &device : devices)
+    {
+        if (device.connected && device.handsfreeTelephonySupported)
+        {
+            std::wcout << L"Attempting to disconnect the Telephony for:" << device.name << std::endl;
+            if (btObj.setHandsfreeTelephonyServiceState(device, false))
+            {
+                std::wcout << L"Handsfree Telephony has been disabled for: " << device.name << std::endl;
+            }
+            else
+            {
+                std::wcout << L"Failed to disable Handsfree Telephony for: " << device.name << std::endl;
+            }
+        }
+
+    }
+
+    return 0;
+}
